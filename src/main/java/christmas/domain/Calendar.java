45b0265181e09631dd82db402a16a3617664a872
yhcho0405 <yhcho0405@kakao.com>
2023-11-10 18:55:08 +0900

    feat: Menu, Calendar enums 구현

---
+++ src/main/java/christmas/domain/Calendar.java
@@ -0,0 +1,79 @@
+package christmas.domain;
+
+import java.util.Set;
+
+public enum Calendar {
+    DAY_1(1, DayType.WEEKEND),
+    DAY_2(2, DayType.WEEKEND),
+    DAY_3(3, DayType.WEEKDAY, true),
+    DAY_4(4, DayType.WEEKDAY),
+    DAY_5(5, DayType.WEEKDAY),
+    DAY_6(6, DayType.WEEKDAY),
+    DAY_7(7, DayType.WEEKDAY),
+    DAY_8(8, DayType.WEEKEND),
+    DAY_9(9, DayType.WEEKEND),
+    DAY_10(10, DayType.WEEKDAY, true),
+    DAY_11(11, DayType.WEEKDAY),
+    DAY_12(12, DayType.WEEKDAY),
+    DAY_13(13, DayType.WEEKDAY),
+    DAY_14(14, DayType.WEEKDAY),
+    DAY_15(15, DayType.WEEKEND),
+    DAY_16(16, DayType.WEEKEND),
+    DAY_17(17, DayType.WEEKDAY, true),
+    DAY_18(18, DayType.WEEKDAY),
+    DAY_19(19, DayType.WEEKDAY),
+    DAY_20(20, DayType.WEEKDAY),
+    DAY_21(21, DayType.WEEKDAY),
+    DAY_22(22, DayType.WEEKEND),
+    DAY_23(23, DayType.WEEKEND),
+    DAY_24(24, DayType.WEEKDAY, true),
+    DAY_25(25, DayType.WEEKDAY, true),
+    DAY_26(26, DayType.WEEKDAY),
+    DAY_27(27, DayType.WEEKDAY),
+    DAY_28(28, DayType.WEEKDAY),
+    DAY_29(29, DayType.WEEKEND),
+    DAY_30(30, DayType.WEEKEND),
+    DAY_31(31, DayType.WEEKDAY, true);
+
+    private static final Set<Integer> SPECIAL_DISCOUNT_DAYS = Set.of(3, 10, 17, 24, 25, 31);
+
+    private final int day;
+    private final DayType dayType;
+    private final boolean isSpecialDiscountDay;
+
+    Calendar(int day, DayType dayType) {
+        this(day, dayType, false);
+    }
+
+    Calendar(int day, DayType dayType, boolean isSpecialDiscountDay) {
+        this.day = day;
+        this.dayType = dayType;
+        this.isSpecialDiscountDay = isSpecialDiscountDay;
+    }
+
+
+    public static Calendar getByDay(int day) {
+        for (Calendar calendar : Calendar.values()) {
+            if (calendar.day == day) {
+                return calendar;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR]");
+    }
+
+    public boolean isWeekend() {
+        return dayType == DayType.WEEKEND;
+    }
+
+    public boolean isSpecialDiscountDay() {
+        return isSpecialDiscountDay;
+    }
+
+    public int daysUntilChristmas() {
+        return 25 - this.day;
+    }
+
+    private enum DayType {
+        WEEKDAY, WEEKEND
+    }
+}
---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,51 @@
+package christmas.domain;
+
+public enum Menu {
+    MUSHROOM_SOUP("양송이수프", 6_000, Category.APPETIZER),
+    TAPAS("타파스", 5_500, Category.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, Category.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55_000, Category.MAIN),
+    BBQ_RIBS("바비큐립", 54_000, Category.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35_000, Category.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, Category.MAIN),
+    CHOCO_CAKE("초코케이크", 15_000, Category.DESSERT),
+    ICE_CREAM("아이스크림", 5_000, Category.DESSERT),
+    ZERO_COLA("제로콜라", 3_000, Category.BEVERAGE),
+    RED_WINE("레드와인", 60_000, Category.BEVERAGE),
+    CHAMPAGNE("샴페인", 25_000, Category.BEVERAGE);
+
+    private final String name;
+    private final int price;
+    private final Category category;
+
+    Menu(String name, int price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public static Menu getByName(String menuName) {
+        for (Menu menu : Menu.values()) {
+            if (menu.getName().equals(menuName)) {
+                return menu;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR]");
+    }
+
+    public enum Category {
+        APPETIZER, MAIN, DESSERT, BEVERAGE
+    }
+}
